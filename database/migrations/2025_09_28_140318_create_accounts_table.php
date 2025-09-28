d8defd367dc8f91ffc7ac1727a9a5b324aacdec7
YassineAitHadou1 <yassineflifla456@gmail.com>
2025-09-28 15:52:11 +0100

    failed accounts factory create

---
+++ database/migrations/2025_09_28_140318_create_accounts_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('accountName');
+            $table->string('accountEmail');
+            $table->string('accountPassword');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('accounts');
+    }
+};
