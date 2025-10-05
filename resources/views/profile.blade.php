80e063e7a2a2ab5f003f3bcee4699f1fa72828e4
YassineAitHadou1 <yassineflifla456@gmail.com>
2025-10-05 15:37:59 +0100

    discover seeders

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -1,5 +1,5 @@
     @section('title') profile @endsection
-    @extends('layouts.master');
+
     @section('main')
     <h1>profile page</h1>
     @endsection
--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -1,5 +1,5 @@
     @section('title') settings @endsection
-    @extends('layouts.master');
+
     @section('main')
     <h1>settings page</h1>
     @endsection
